52bcaf06bf1b8e16e28f3f1f3dd9d31fdca72e8f
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-08 20:33:53 +0100

    bumped copyright year

--- classes/BBCode.php
+++ classes/BBCode.php
@@ -8,7 +8,7 @@
  * @category  CMSimple_XH
  * @package   Forum
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2013-2014 Christoph M. Becker <http://3-magi.net/>
+ * @copyright 2013-2015 Christoph M. Becker <http://3-magi.net/>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @version   SVN: $Id$
  * @link      http://3-magi.net/?CMSimple_XH/Forum_XH
