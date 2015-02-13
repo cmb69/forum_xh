93059ea4db71ab214d865c6770356ccc414f1ccc
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-13 13:52:34 +0100

    added Russion localization (contributed by Old)

---
+++ languages/ru.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Old aka Lybomyr Kydray
+ *
+ * Translation are licensed under the GNU General Public License, version 2 or
+ * later.
+ */
+
+$plugin_tx['forum']['format_date']="j F Y";
+$plugin_tx['forum']['format_time']="H:i:s";
+$plugin_tx['forum']['msg_start_topic']="Начать новую тему";
+$plugin_tx['forum']['msg_back']="Вернуться к темам";
+$plugin_tx['forum']['msg_add_comment']="Добавить комментарий";
+$plugin_tx['forum']['msg_new_topic']="Новая тема";
+$plugin_tx['forum']['msg_topics']="Темы";
+$plugin_tx['forum']['msg_posted']=" <strong>{user}</strong>, {date} в {time}";
+$plugin_tx['forum']['msg_topic_details']="{comments} Новый: {posted}.";
+$plugin_tx['forum']['msg_comments_singular']="Комментрай %d.";
+$plugin_tx['forum']['msg_comments_plural_2-4']="Всего комментариев: %d.";
+$plugin_tx['forum']['msg_comments_plural_5']="Комментариев: %d.";
+$plugin_tx['forum']['msg_confirm_delete']="Комментарий будет удален!";
+$plugin_tx['forum']['msg_title']="Название темы";
+$plugin_tx['forum']['msg_invalid_name']="Неправильное имя форума (разрешены только a-z,  0-9 и \"-\"):";
+$plugin_tx['forum']['msg_powered_by']="Предоставлено <a href=\"http://3-magi.net/?CMSimple_XH/Forum_XH\">Forum_XH</a>";
+$plugin_tx['forum']['lbl_title_missing']="Введите название темы";
+$plugin_tx['forum']['lbl_comment_missing']="Введите свой комментарий.";
+$plugin_tx['forum']['lbl_submit']="Добавить";
+$plugin_tx['forum']['lbl_delete']="Удалить комментарий";
+$plugin_tx['forum']['lbl_bold']="Полужирный";
+$plugin_tx['forum']['lbl_italic']="Наклонный";
+$plugin_tx['forum']['lbl_underline']="Подчеркнутый";
+$plugin_tx['forum']['lbl_emoticon']="Иконки эмоций";
+$plugin_tx['forum']['lbl_smile']="Улыбающийся";
+$plugin_tx['forum']['lbl_wink']="Подмигивающий";
+$plugin_tx['forum']['lbl_happy']="Счастливый";
+$plugin_tx['forum']['lbl_grin']="Ухмыляющийся";
+$plugin_tx['forum']['lbl_tongue']="Кривляющийся";
+$plugin_tx['forum']['lbl_surprised']="Удивленный";
+$plugin_tx['forum']['lbl_unhappy']="Несчастный";
+$plugin_tx['forum']['lbl_picture']="Изображение";
+$plugin_tx['forum']['lbl_link']="Ссылка";
+$plugin_tx['forum']['lbl_size']="Размер";
+$plugin_tx['forum']['lbl_big']="Большой";
+$plugin_tx['forum']['lbl_normal']="Обычный";
+$plugin_tx['forum']['lbl_small']="Меньший";
+$plugin_tx['forum']['lbl_bulleted_list']="Маркированный список";
+$plugin_tx['forum']['lbl_numeric_list']="Нумерованный список";
+$plugin_tx['forum']['lbl_list_item']="Элемент списка";
+$plugin_tx['forum']['lbl_quotes']="Цитата";
+$plugin_tx['forum']['lbl_code']="Код";
+$plugin_tx['forum']['lbl_clean']="Очистить";
+$plugin_tx['forum']['lbl_preview']="Просмотр";
+$plugin_tx['forum']['lbl_link_text']="Введите текст для ссылки ...";
+$plugin_tx['forum']['syscheck_title']="Проверка системы";
+$plugin_tx['forum']['syscheck_phpversion']="Версия PHP &ge; %s";
+$plugin_tx['forum']['syscheck_extension']="Расширение '%s' загружено";
+$plugin_tx['forum']['syscheck_encoding']="Кодировка 'UTF-8' установлена";
+$plugin_tx['forum']['syscheck_magic_quotes']="Экранирование спецсимволов (\"magic_quotes_runtime\") отключено";
+$plugin_tx['forum']['syscheck_jquery']="Плагин <a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple</a> установлен";
+$plugin_tx['forum']['syscheck_writable']="Папка '%s' доступна для записи";
+$plugin_tx['forum']['cf_folder_data']="Путь к папке, где хранятся файлы данных, относительно корневого каталога CMSimple. Например, \"userfiles/forum/\". Оставьте поле пустым для хранения данных в папке по умолчанию (каталог <b>data/</b> в директории плагина).";
+$plugin_tx['forum']['cf_format_date']="Подробнее о форматировании даты читайте на http://php.net/manual/en/function.date.php.";
+$plugin_tx['forum']['cf_format_time']="Подробнее о форматировании времени читайте на http://php.net/manual/en/function.date.php.";
+
+?>
