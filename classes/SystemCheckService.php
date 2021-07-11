c761e07ff77d4ebffa03ba28fea34c414c5455bc
Christoph M. Becker <cmbecker69@gmx.de>
2021-07-11 14:59:45 +0200

    Convert stdClass objects to arrays
    The latter are not particularly good, but the former are an
    abomination, and should never be used.

--- classes/SystemCheckService.php
+++ classes/SystemCheckService.php
@@ -54,7 +54,7 @@ class SystemCheckService
     }
 
     /**
-     * @return object[]
+     * @return array<int,array>string,mixed>>
      */
     public function getChecks()
     {
@@ -73,62 +73,62 @@ class SystemCheckService
 
     /**
      * @param string $version
-     * @return object
+     * @return array<string,mixed>
      */
     private function checkPhpVersion($version)
     {
         $state = version_compare(PHP_VERSION, $version, 'ge') ? 'success' : 'fail';
         $label = sprintf($this->lang['syscheck_phpversion'], $version);
         $stateLabel = $this->lang["syscheck_$state"];
-        return (object) compact('state', 'label', 'stateLabel');
+        return compact('state', 'label', 'stateLabel');
     }
 
     /**
      * @param string $extension
      * @param bool $isMandatory
-     * @return object
+     * @return array<string,mixed>
      */
     private function checkExtension($extension, $isMandatory = true)
     {
         $state = extension_loaded($extension) ? 'success' : ($isMandatory ? 'fail' : 'warning');
         $label = sprintf($this->lang['syscheck_extension'], $extension);
         $stateLabel = $this->lang["syscheck_$state"];
-        return (object) compact('state', 'label', 'stateLabel');
+        return compact('state', 'label', 'stateLabel');
     }
 
     /**
      * @param string $version
-     * @return object
+     * @return array<string,mixed>
      */
     private function checkXhVersion($version)
     {
         $state = version_compare(CMSIMPLE_XH_VERSION, "CMSimple_XH $version", 'ge') ? 'success' : 'fail';
         $label = sprintf($this->lang['syscheck_xhversion'], $version);
         $stateLabel = $this->lang["syscheck_$state"];
-        return (object) compact('state', 'label', 'stateLabel');
+        return compact('state', 'label', 'stateLabel');
     }
 
     /**
      * @param string $plugin
-     * @return object
+     * @return array<string,mixed>
      */
     private function checkPlugin($plugin)
     {
         $state = is_dir($this->pluginsFolder) ? 'success' : 'fail';
         $label = sprintf($this->lang['syscheck_plugin'], $plugin);
         $stateLabel = $this->lang["syscheck_$state"];
-        return (object) compact('state', 'label', 'stateLabel');
+        return compact('state', 'label', 'stateLabel');
     }
 
     /**
      * @param string $folder
-     * @return object
+     * @return array<string,mixed>
      */
     private function checkWritability($folder)
     {
         $state = is_writable($folder) ? 'success' : 'warning';
         $label = sprintf($this->lang['syscheck_writable'], $folder);
         $stateLabel = $this->lang["syscheck_$state"];
-        return (object) compact('state', 'label', 'stateLabel');
+        return compact('state', 'label', 'stateLabel');
     }
 }
--- views/info.php
+++ views/info.php
@@ -2,13 +2,13 @@
 /**
  * @var \Forum\View $this
  * @var string $version
- * @var array<int,object> $checks
+ * @var array<int,array<string,mixed>> $checks
  */
 ?>
 <h1>Forum <?=$this->esc($version)?></h1>
 <div class="forum_syscheck">
     <h2><?=$this->text('syscheck_title')?></h2>
 <?php foreach ($checks as $check):?>
-    <p class="xh_<?=$this->esc($check->state)?>"><?=$this->text('syscheck_message', $check->label, $check->stateLabel)?></p>
+    <p class="xh_<?=$this->esc($check['state'])?>"><?=$this->text('syscheck_message', $check['label'], $check['stateLabel'])?></p>
 <?php endforeach?>
 </div>
